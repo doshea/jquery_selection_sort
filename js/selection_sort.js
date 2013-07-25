8249466482963b30bfa987a80c7e78f0c8dac298
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-25 04:33:17 -0400

    Fetching text without HTML tags and adding a css file

--- js/selection_sort.js
+++ js/selection_sort.js
@@ -1,12 +1,9 @@
 var cleaned_text;
 
 function grab_and_clean_text(callback){
-  $.getJSON('http://hipsterjesus.com/api/', function(data) {
+  $.getJSON('http://hipsterjesus.com/api/?type=hipster-centric&html=false', function(data) {
     //Remove periods
     var hipster_text = data.text.replace(/\./g, "");
-    //Remove start and end <p> tags
-    hipster_text = hipster_text.replace(/<p>/g, "");
-    hipster_text = hipster_text.replace(/<\/p>/g, "");
     //Replace line breaks with newlines
     hipster_text = hipster_text.replace(/(\r\n|\n|\r)/gm," ");
     //Remove duplicate spaces
